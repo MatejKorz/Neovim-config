dc01d1701c75ad568652df7b98733677cf5138c7
MatejKorz <MateKorz@github.com>
2023-12-14 21:44:21 +0100

    Colorscheme setup

--- lua/xkorz/plugins/lualine.lua
+++ lua/xkorz/plugins/lualine.lua
@@ -1,26 +1,26 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        local lualine = require("lualine")
-        local lazy_status = require("lazy.status")
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    local lualine = require("lualine")
+    local lazy_status = require("lazy.status")
 
-        lualine.setup({
-            options = {
-                globalstatus = true,
-            },
-            sections = {
-                lualine_x = {
-                    {
-                        lazy_status.updates,
-                        cond = lazy_status.has_updates,
-                    },
-                    {"encoding"},
-                    {"fileformat"},
-                    {"filetype"},
-                },
-            },
-        })
-
-    end
+    lualine.setup({
+      options = {
+        theme = "catppuccin",
+        globalstatus = true,
+      },
+      sections = {
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+          },
+          {"encoding"},
+          {"fileformat"},
+          {"filetype"},
+        },
+      },
+    })
+  end
 }
